1689321d7862cab849d2d47d0aaba17f8c30f92a
Balkrishna6420 <111333330+Balkrishna6420@users.noreply.github.com>
2025-04-02 23:30:10 +0530

    Time: 0 ms (100%), Space: 9.9 MB (57.98%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,60 +1,26 @@
-#include<string>
-#include<iostream>
-#include<vector>
-using namespace std;
 class Solution {
-private:
-    bool valid(char ch) {
-        if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
-            return 1;
-        }
-        
-        return 0;
-    }
-    
-    char toLowerCase(char ch) {
-    if( (ch >='a' && ch <='z') || (ch >='0' && ch <='9') )
-        return ch;
-    else{
-        char temp = ch - 'A' + 'a';
-        return temp;
-    }
-}
-    bool checkPalindrome(string a) {
-    int s = 0;
-    int e = a.length()-1;
-
-    while(s<=e) {
-        if(a[s] != a[e])
-        {
-            return 0;       
-        }
-        else{
-            s++;
-            e--;
-        }
-    }
-    return 1;
-}
 public:
     bool isPalindrome(string s) {
-        
-        //faltu character hatado
-        string temp = "";
-        
-        for(int j=0; j<s.length(); j++) {   
-            if(valid(s[j])) {
-                temp.push_back(s[j]);
-            }
+       return checkPalindrome(s,0,s.length()-1); 
+    }
+
+private:
+bool checkPalindrome(string& s,int start,int end) {
+        //base case
+        if(start>end){
+            return true;
         }
-        
-        //lowercase me kardo
-        for(int j=0; j<temp.length(); j++) { 
-            temp[j] = toLowerCase(temp[j]);
+        if(s==" "){
+            return true;
         }
-        
-        //check palindrome
-        return checkPalindrome(temp);
-        
+        // Skip non-alphanumeric characters
+        while (start < end && !isalnum(s[start])) start++;
+        while (start < end && !isalnum(s[end])) end--;
+        if(tolower (s[start])!=tolower (s[end])){
+            return false;
+        }else{
+            //recursion
+            return checkPalindrome(s,start+1,end-1);
+        } 
     }
 };
