c73915182052397463af1574a04e622fefb90f07
Balkrishna6420 <111333330+Balkrishna6420@users.noreply.github.com>
2026-06-12 19:44:10 +0530

    Time: 63 ms (61.38%), Space: 276.3 MB (44.58%) - LeetHub

---
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+int solve(vector<int>&obstacles,int currLane,int currPos,vector<vector<int>>&dp){
+    //base case
+    int n=obstacles.size()-1;
+    if(currPos==n){
+        return 0;
+    }
+    if(dp[currLane][currPos]!=-1){
+        return dp[currLane][currPos];
+    }
+    if(obstacles[currPos+1]!=currLane){
+        return solve(obstacles,currLane,currPos+1,dp);
+    }
+    else{
+        //side ways jump
+        int ans=INT_MAX;
+        for(int i=1;i<=3;i++){
+            if(currLane!=i && obstacles[currPos]!=i){
+                ans=min(ans,1+solve(obstacles,i,currPos,dp));
+            }
+        }
+        dp[currLane][currPos]=ans;
+        return dp[currLane][currPos];
+    }
+}
+    int minSideJumps(vector<int>& obstacles) {
+        vector<vector<int>>dp(4,vector<int>(obstacles.size(),-1));
+        return solve(obstacles,2,0,dp);
+    }
+};
