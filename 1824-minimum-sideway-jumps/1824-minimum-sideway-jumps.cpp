6ce368f12305f8851b8fd8cdcbedcae3632da9b8
Balkrishna6420 <111333330+Balkrishna6420@users.noreply.github.com>
2026-06-12 20:20:45 +0530

    Time: 70 ms (54.47%), Space: 191.6 MB (96.86%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -2,28 +2,30 @@ class Solution {
 public:
 int solve(vector<int>&obstacles){
     int n=obstacles.size()-1;
-    vector<vector<int>>dp(4,vector<int>(obstacles.size(),1e9));
-    dp[0][n]=0;
-    dp[1][n]=0;
-    dp[2][n]=0;
-    dp[3][n]=0;
+    vector<int>curr(4,INT_MAX);
+    vector<int>next(4,INT_MAX);
+    next[0]=0;
+    next[1]=0;
+    next[2]=0;
+    next[3]=0;
     for(int currPos=n-1;currPos>=0;currPos--){
         for(int currLane=1;currLane<=3;currLane++){
             if(obstacles[currPos+1]!=currLane){
-                dp[currLane][currPos]=dp[currLane][currPos+1];
+                curr[currLane]=next[currLane];
             }else{
                 //sideways jump
                 int ans=1e9;
                 for(int i=1;i<=3;i++){
                     if(currLane!=i && obstacles[currPos]!=i){
-                        ans=min(ans,1+dp[i][currPos+1]);
+                        ans=min(ans,1+next[i]);
                     }
                 }
-                dp[currLane][currPos]=ans;
+                curr[currLane]=ans;
             }
         }
+        next=curr;
     }
-    return min(dp[2][0],min(1+dp[1][0],1+dp[3][0]));
+    return min(next[2],min(1+next[1],1+next[3]));
 }
     int minSideJumps(vector<int>& obstacles) {
         return solve(obstacles);
